cfff7a6a41a7c1ba6321fe14040e0c9d2668d236
Rashmi-Salunkhe <158038788+Rashmi-Salunkhe@users.noreply.github.com>
2024-09-02 21:28:31 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,13 @@
-//your JS code here. If required.
 function allMethods() {
-  //write your code here
+    // Get all properties of the Math object
+    const properties = Object.getOwnPropertyNames(Math);
+    
+    // Filter out only the methods (functions)
+    const methods = properties.filter(property => typeof Math[property] === 'function');
+    
+    // Join the method names into a comma-separated string
+    return methods.join(', ');
 }
 
+// Display the methods in an alert box
 alert(allMethods());
